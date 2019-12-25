74922a6f8f115869cacf90a6e54255b92d0df907
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-12-25 03:08:39 -0500

    added a simple class to init the firebase playground

--- lib/playground/firebase_playground.rb
+++ lib/playground/firebase_playground.rb
@@ -3,3 +3,10 @@ Dotenv.load
 
 require "google/cloud/firestore"
 
+module Playground
+  class FirebasePlayground
+    def initialize(config)
+      @firestore = Google::Cloud::Firestore.new project_id: config.project_id
+    end
+  end
+end
