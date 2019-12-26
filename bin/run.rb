d622ced9dcbc85c40dce14d5ce4c1cce02083303
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-12-26 03:35:14 -0500

    ran rubocop and switched to using require_relative

--- bin/run.rb
+++ bin/run.rb
@@ -1,10 +1,16 @@
+# frozen_string_literal: true
+
 require 'dotenv'
 Dotenv.load
 
-require 'playground/firebase_playground'
+require_relative '../lib/playground/firebase_playground'
 
 firebase_config = {
-  project_id: ENV.PROJECT_ID
+  project_id: ENV['PROJECT_ID']
 }
 
-playground = Playground::FirebasePlayground.new firebase_config
+# playground = Playground::FirebasePlayground.new firebase_config
+
+puts firebase_config
+
+puts 'run complete!'
