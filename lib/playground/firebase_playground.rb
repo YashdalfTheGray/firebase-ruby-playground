39ddb0b83c9891286043fcae888f950906637537
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-12-28 01:02:26 -0500

    ran rubocop and fixed the firestore code

--- lib/playground/firebase_playground.rb
+++ lib/playground/firebase_playground.rb
@@ -1,12 +1,16 @@
+# frozen_string_literal: true
+
 require 'dotenv'
 Dotenv.load
 
-require "google/cloud/firestore"
+require 'google/cloud/firestore'
 
 module Playground
+  # FirebasePlayground holds a firestore reference and is able to
+  # act on it.
   class FirebasePlayground
     def initialize(config)
-      @firestore = Google::Cloud::Firestore.new project_id: config.project_id
+      @firestore = Google::Cloud::Firestore.new project_id: config[:project_id]
     end
   end
 end
