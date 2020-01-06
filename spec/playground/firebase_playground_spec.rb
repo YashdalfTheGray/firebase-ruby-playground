2983f61f50e4ff6d1f431711634887d0896e5d4d
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-01-06 00:55:46 -0800

    do not run the firestore init tests if under ci

--- spec/playground/firebase_playground_spec.rb
+++ spec/playground/firebase_playground_spec.rb
@@ -3,7 +3,7 @@
 require 'playground/firebase_playground'
 
 module Playground
-  describe FirebasePlayground do
+  describe FirebasePlayground, unless: ENV['CI'] == 'true' do
     it 'news up a new playground' do
       playground = FirebasePlayground.new(project_id: ENV['project_id'])
       expect(playground).to be_truthy
