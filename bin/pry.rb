f8ac0d26320af003455feda7755480c4d598bc36
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-01-10 02:00:59 -0800

    added the project config to the pry "binary" too

--- bin/pry.rb
+++ bin/pry.rb
@@ -8,4 +8,8 @@ require 'pry'
 require_relative '../lib/playground/firebase_playground'
 require_relative '../lib/playground/models/contact.rb'
 
+firebase_config = {
+  project_id: ENV['PROJECT_ID'],
+}
+
 binding.pry # rubocop:disable Lint/Debugger
