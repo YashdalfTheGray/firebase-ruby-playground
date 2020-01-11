e3acc9492bcd1bfa471527d920379fa786ceda1d
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-01-11 05:27:11 -0800

    moved the firebase config above the require statements

--- bin/pry.rb
+++ bin/pry.rb
@@ -5,11 +5,11 @@ Dotenv.load
 
 require 'pry'
 
-require_relative '../lib/playground/firebase_playground'
-require_relative '../lib/playground/models/contact.rb'
-
 firebase_config = {
   project_id: ENV['PROJECT_ID'],
 }
+
+require_relative '../lib/playground/firebase_playground'
+require_relative '../lib/playground/models/contact.rb'
 
 binding.pry # rubocop:disable Lint/Debugger
