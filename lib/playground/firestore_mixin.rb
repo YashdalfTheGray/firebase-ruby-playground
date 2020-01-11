47f3cf808a1e992930d7c2c30f00fd2ee1349a10
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-01-11 05:28:44 -0800

    added a firestore mixin to hold all of the data methods

---
+++ lib/playground/firestore_mixin.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Playground
+  # FirestoreMixin holds all of the oeprations that
+  # we can perform on the data
+  module FirestoreMixin
+    def read(collection, doc)
+      document_ref = @firestore.doc "#{collection}/#{doc.id}"
+      begin
+        document_ref.set doc.serialize
+      rescue NoMethodError
+        raise ArgumentError 'Document is not serializable'
+      end
+    end
+  end
+end
