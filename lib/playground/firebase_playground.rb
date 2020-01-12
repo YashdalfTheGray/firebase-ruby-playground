e04a9294465e9fca8ee4bf4df21e79a22e05cd46
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-01-12 00:20:32 -0800

    added the firestore mixin to the firebase playground

--- lib/playground/firebase_playground.rb
+++ lib/playground/firebase_playground.rb
@@ -5,10 +5,14 @@ Dotenv.load
 
 require 'google/cloud/firestore'
 
+require_relative 'firestore_mixin'
+
 module Playground
   # FirebasePlayground holds a firestore reference and is able to
   # act on it.
   class FirebasePlayground
+    include FirestoreMixin
+
     def initialize(config)
       @firestore = Google::Cloud::Firestore.new project_id: config[:project_id]
     end
