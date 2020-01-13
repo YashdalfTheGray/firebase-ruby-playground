2c5fef1517d51c83179fc0d90f2da0a2d1fd77ec
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-01-13 01:00:20 -0800

    added a test to check for raised error if document that can't be serialized

--- spec/playground/firestore_mixin_spec.rb
+++ spec/playground/firestore_mixin_spec.rb
@@ -17,5 +17,33 @@ module Playground
     end
 
     let(:dummy) { FirestoreMixinTestDummy.new instance_double(Google::Cloud::Firestore::Client) }
+
+    context '.write' do
+      it 'serializes a document into the firestore' do
+        firestore_doc_double = instance_double(Google::Cloud::Firestore::DocumentReference)
+        data_doc_double = instance_double(Playground::Models::Contact)
+
+        expect(dummy.firestore).to receive('doc').and_return firestore_doc_double
+        # If necessary, it can return Google::Cloud::Firestore::CommitResponse::WriteResult
+        expect(firestore_doc_double).to receive('set').and_return true
+        expect(data_doc_double).to receive('serialize').and_return({})
+        expect(data_doc_double).to receive('id').and_return 'data_doc_double_id'
+
+        expect(dummy.write('contacts', data_doc_double)).to eq true
+      end
+
+      it 'raises an error if the document cannot be serialized' do
+        firestore_doc_double = instance_double(Google::Cloud::Firestore::DocumentReference)
+        data_doc_double = instance_double(Playground::Models::Contact)
+
+        expect(dummy.firestore).to receive('doc').and_return firestore_doc_double
+        # If necessary, it can return Google::Cloud::Firestore::CommitResponse::WriteResult
+        expect(firestore_doc_double).not_to receive 'set'
+        expect(data_doc_double).to receive('serialize').and_raise NoMethodError
+        expect(data_doc_double).to receive('id').and_return 'data_doc_double_id'
+
+        expect { dummy.write('contacts', data_doc_double) }.to raise_error ArgumentError
+      end
+    end
   end
 end
