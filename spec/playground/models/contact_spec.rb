24f41034b5e940ffe720c288fdf3fd2679bd31d7
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-01-14 00:36:36 -0800

    added contexts within the contact object test suite

--- spec/playground/models/contact_spec.rb
+++ spec/playground/models/contact_spec.rb
@@ -24,48 +24,52 @@ module Playground
         expect(test_contact.updated_at).to be_a(DateTime)
       end
 
-      it 'has the ability to be converted into a hash' do
-        expect(test_contact.to_h).to have_key 'name'
-        expect(test_contact.to_h).to have_key 'email'
-        expect(test_contact.to_h).to have_key 'phone'
-        expect(test_contact.to_h).to have_key 'id'
-        expect(test_contact.to_h).to have_key 'created_at'
-        expect(test_contact.to_h).to have_key 'updated_at'
+      context 'properties' do
+        it 'tracks the updated datetime when updating name' do
+          original_update_time = test_contact.updated_at
+          sleep 0.01 # so that udpate times are different
+          test_contact.name = 'Guybrush Threepwood'
+          expect(test_contact.name).to eq 'Guybrush Threepwood'
+          expect(test_contact.updated_at).to happen_after original_update_time
+        end
+
+        it 'tracks the updated datetime when updating email' do
+          original_update_time = test_contact.updated_at
+          sleep 0.01 # so that udpate times are different
+          test_contact.email = 'guybrush@threepwood.io'
+          expect(test_contact.email).to eq 'guybrush@threepwood.io'
+          expect(test_contact.updated_at).to happen_after original_update_time
+        end
+
+        it 'throws an error if given an incorrect email format' do
+          expect { test_contact.email = 'stuff' }.to raise_error ArgumentError
+        end
+
+        it 'tracks the updated datetime when updating phone' do
+          original_update_time = test_contact.updated_at
+          sleep 0.01 # so that udpate times are different
+          test_contact.phone = '888-867-5309'
+          expect(test_contact.phone).to eq '888-867-5309'
+          expect(test_contact.updated_at).to happen_after original_update_time
+        end
       end
 
-      it 'tracks the updated datetime when updating name' do
-        original_update_time = test_contact.updated_at
-        sleep 0.01 # so that udpate times are different
-        test_contact.name = 'Guybrush Threepwood'
-        expect(test_contact.name).to eq 'Guybrush Threepwood'
-        expect(test_contact.updated_at).to happen_after original_update_time
-      end
+      context 'serialization' do
+        it 'has the ability to be converted into a hash' do
+          expect(test_contact.to_h).to have_key 'name'
+          expect(test_contact.to_h).to have_key 'email'
+          expect(test_contact.to_h).to have_key 'phone'
+          expect(test_contact.to_h).to have_key 'id'
+          expect(test_contact.to_h).to have_key 'created_at'
+          expect(test_contact.to_h).to have_key 'updated_at'
+        end
 
-      it 'tracks the updated datetime when updating email' do
-        original_update_time = test_contact.updated_at
-        sleep 0.01 # so that udpate times are different
-        test_contact.email = 'guybrush@threepwood.io'
-        expect(test_contact.email).to eq 'guybrush@threepwood.io'
-        expect(test_contact.updated_at).to happen_after original_update_time
-      end
+        it 'can be converted to valid json' do
+          json_string = test_contact.serialize
 
-      it 'throws an error if given an incorrect email format' do
-        expect { test_contact.email = 'stuff' }.to raise_error ArgumentError
-      end
-
-      it 'tracks the updated datetime when updating phone' do
-        original_update_time = test_contact.updated_at
-        sleep 0.01 # so that udpate times are different
-        test_contact.phone = '888-867-5309'
-        expect(test_contact.phone).to eq '888-867-5309'
-        expect(test_contact.updated_at).to happen_after original_update_time
-      end
-
-      it 'can be converted to valid json' do
-        json_string = test_contact.serialize
-
-        expect { JSON.parse(json_string) }.not_to raise_error
-        expect(JSON.parse(json_string)).to eq test_contact.to_h
+          expect { JSON.parse(json_string) }.not_to raise_error
+          expect(JSON.parse(json_string)).to eq test_contact.to_h
+        end
       end
     end
   end
