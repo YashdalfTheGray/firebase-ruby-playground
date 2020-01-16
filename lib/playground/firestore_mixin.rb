76915326b930ec38247a4f152b0297b075c358b2
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-01-16 02:43:54 -0800

    started documenting the project

--- lib/playground/firestore_mixin.rb
+++ lib/playground/firestore_mixin.rb
@@ -4,6 +4,11 @@ module Playground
   # FirestoreMixin holds all of the oeprations that
   # we can perform on the data
   module FirestoreMixin
+    # Write a document that is serializable to the firestore. The document
+    # being stored needs to be able to be JSON serializable.
+    #
+    # @param collection [String] the collection to put this document into
+    # @param doc [Object, #serialize] the object to put in the document
     def write(collection, doc)
       document_ref = @firestore.doc "#{collection}/#{doc.id}"
       begin
