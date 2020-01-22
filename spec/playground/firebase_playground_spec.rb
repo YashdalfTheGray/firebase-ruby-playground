03070497d7aafa9da858f08f56cecb1ed477f887
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-01-22 08:25:58 -0800

    added a describe suite for the firebase config validation

--- spec/playground/firebase_playground_spec.rb
+++ spec/playground/firebase_playground_spec.rb
@@ -9,4 +9,19 @@ module Playground
       expect(playground).to be_truthy
     end
   end
+
+  describe "#{FirebasePlayground} validation" do
+    it 'raises an error for an invalid property name' do
+      expect { FirebasePlayground.new(stuff: 'things') }.to raise_error ArgumentError
+    end
+
+    it 'raises an error for an empty config' do
+      expect { FirebasePlayground.new }.to raise_error ArgumentError
+    end
+
+    it 'raises an error for a config that has additional properties' do
+      config = { project_id: ENV['project_id'], test_key: 'test' }
+      expect { FirebasePlayground.new config }.to raise_error ArgumentError
+    end
+  end
 end
